27ce0fd330db790e0e47a67eba1d42b19de2d9a6
Your Name <ravikumar699121@gmail.com>
2025-07-20 13:56:27 +0000

    // src/config/firebaseConfig.js
    import { initializeApp } from "firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC70iMVmZ2Xskik0zMP8TcUM4d2olBw4TE",
+  authDomain: "computationtax.firebaseapp.com",
+  projectId: "computationtax",
+  storageBucket: "computationtax.firebasestorage.app",
+  messagingSenderId: "413709098206",
+  appId: "1:413709098206:web:be583c64514bdd9590764f"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
