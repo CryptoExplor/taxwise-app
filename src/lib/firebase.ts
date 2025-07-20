7a3687ac480aaba6ad261e360b57325fa4b359ea
Your Name <ravikumar699121@gmail.com>
2025-07-20 13:58:55 +0000

    how to lohin and check app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC70iMVmZ2Xskik0zMP8TcUM4d2olBw4TE",
   authDomain: "computationtax.firebaseapp.com",
   projectId: "computationtax",
-  storageBucket: "computationtax.firebasestorage.app",
+  storageBucket: "computationtax.appspot.com",
   messagingSenderId: "413709098206",
   appId: "1:413709098206:web:be583c64514bdd9590764f"
 };
@@ -17,5 +18,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, db };
+export { app, auth, db, storage };
